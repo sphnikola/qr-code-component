3dddcbb2ac019e97a31bc48ee82202749d7521ac
sphnikola <sphnikola@gmail.com>
2023-09-20 20:16:49 +0100

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors:{
+      transparent : 'transparent',
+      current : 'currentColor',
+      'light-gray' : '#D6E2F0',
+      'grayish-blue' : '#7B879D',
+      'dark-blue' : '#1F3251',
+      'white' : '#FFFFFF'
+    },
+    fontFamily : {
+      outline : ['Outfit','sans-serif']
+    }, 
+  },
+  plugins: [],
+}
+
